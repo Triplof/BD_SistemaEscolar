dda3724092acdbf411dce4eb5705e8082f1e5aeb
Felipe Furlan <triplofff@yahoo.com.br>
2026-05-04 14:29:16 -0300

    Simplificação do cenario

--- sql/Operacoes/Cenario2-Commit.sql
+++ sql/Operacoes/Cenario2-Commit.sql
@@ -4,62 +4,33 @@ USE gestao_escolar;
 --  Objetivo: provar que a transação confirmada é persistida
 
 -- Verificação ANTES da transação
-SELECT 
-    m.pk_mensalidade,
-    m.fk_aluno,
-    CONCAT(p.nome, ' ', p.sobrenome) AS aluno,
-    m.num_parcela,
-    m.valor_liquido,
-    m.status_pagamento
-FROM tb_mensalidades m
-JOIN tb_alunos a  ON m.fk_aluno = a.fk_pessoa
-JOIN tb_pessoa p  ON a.fk_pessoa = p.pk_pessoa
-WHERE m.fk_aluno = 42
-ORDER BY m.pk_mensalidade DESC
-LIMIT 5;
+SELECT COUNT(*) 
+FROM tb_mensalidades 
+WHERE fk_aluno = 41;
 
 START TRANSACTION;
 
-    INSERT INTO tb_mensalidades
-        (fk_aluno, fk_contrato, fk_unidade, num_parcela,
-         valor_bruto, valor_desconto_aplicado, valor_liquido,
-         data_vencimento, status_pagamento)
-    VALUES
-        (42, NULL, 1, 5,
-         5858.00, 0.00, 5858.00,
-         '2026-05-10', 'Pendente');
-         SELECT 
-			m.pk_mensalidade,
-			m.fk_aluno,
-			CONCAT(p.nome, ' ', p.sobrenome) AS aluno,
-			m.num_parcela,
-			m.valor_liquido,
-			m.status_pagamento
-		FROM tb_mensalidades m
-		JOIN tb_alunos a  ON m.fk_aluno = a.fk_pessoa
-		JOIN tb_pessoa p  ON a.fk_pessoa = p.pk_pessoa
-		WHERE m.fk_aluno = 42
-		ORDER BY m.pk_mensalidade DESC
-		LIMIT 5;
+INSERT INTO tb_mensalidades
+    (fk_aluno, fk_unidade, num_parcela,
+     valor_bruto, valor_desconto_aplicado, valor_liquido,
+     data_vencimento, status_pagamento)
+VALUES
+    (41, 1, 99, 1000.00, 0.00, 1000.00, '2026-05-10', 'Pendente');
 
+-- Durante
+
+SELECT COUNT(*) 
+FROM tb_mensalidades 
+WHERE fk_aluno = 41;
 
 COMMIT;
-
 ROLLBACK;
--- Verificação APÓS o COMMIT
-SELECT 
-    m.pk_mensalidade,
-    m.fk_aluno,
-    CONCAT(p.nome, ' ', p.sobrenome) AS aluno,
-    m.num_parcela,
-    m.valor_liquido,
-    m.status_pagamento
-FROM tb_mensalidades m
-JOIN tb_alunos a  ON m.fk_aluno = a.fk_pessoa
-JOIN tb_pessoa p  ON a.fk_pessoa = p.pk_pessoa
-WHERE m.fk_aluno = 42
-ORDER BY m.pk_mensalidade DESC
-LIMIT 5;
+
+-- Depois
+SELECT COUNT(*) 
+FROM tb_mensalidades 
+WHERE fk_aluno = 41;
+-- Resultado esperado: A tabela deve manter novo dado
 
 -- ✔ Conclusão Cenário 2:
 --   O COMMIT confirmou e persistiu a operação.
